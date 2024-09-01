52f377730a31542774ef3dff65ba0024e76b22bb
Matheus Gabriel Valenga <theusgvalenga@outlook.com>
2024-09-01 20:23:40 -0300

    feat: Create service to find one stock product
    Created a new service to find one stock product with id.

--- src/stock_products/stock_products.service.ts
+++ src/stock_products/stock_products.service.ts
@@ -1,10 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductsService } from 'src/products/products.service';
+import { Repository } from 'typeorm';
 import { CreateStockProductDto } from './dto/create-stock_product.dto';
 import { UpdateStockProductDto } from './dto/update-stock_product.dto';
 import { StockProduct } from './entities/stock_product.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { ProductsService } from 'src/products/products.service';
 
 @Injectable()
 export class StockProductsService {
@@ -31,8 +31,17 @@ export class StockProductsService {
     return this.stockProductsRepository.find({ relations: { product: true } });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} stockProduct`;
+  async findOne(id: number) {
+    const foundStockProduct = await this.stockProductsRepository.findOne({
+      where: { id },
+      relations: { product: true },
+    });
+
+    if (!foundStockProduct) {
+      throw new NotFoundException(`Stock Product with ID ${id} was not found`);
+    }
+
+    return foundStockProduct;
   }
 
   update(id: number, updateStockProductDto: UpdateStockProductDto) {
