d2dfd3e1c95bf900a0bb0e1e8641758e819c544d
Matheus Gabriel Valenga <theusgvalenga@outlook.com>
2024-09-12 22:30:29 -0300

    docs(swagger): configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,31 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Products API')
+    .setDescription('A products CRUD API.')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        description: 'Enter a token',
+        in: 'header',
+      },
+      'AUTH_KEY',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
